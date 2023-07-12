10b70b4cfc73a1821f1ecfd931632fe5d5e7e043
MARIUCHINAS <mariususaitis@gmail.com>
2023-07-12 11:07:20 +0300

    Start Button

---
+++ Scenes/hud.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+signal game_started
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func hide_stuff():
+	for i in get_children():
+		i.hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_button_up():
+	hide_stuff()
+	emit_signal("game_started")
+	
