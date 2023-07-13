27bd29112a601ae34a0a948ac3972b1d234f1764
MARIUCHINAS <mariususaitis@gmail.com>
2023-07-13 21:46:06 +0300

    Health and Zombies

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -2,22 +2,30 @@ extends CanvasLayer
 
 signal game_started
 
-var health = 5.000
+var health = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$HealthBar.hide()
 
 func hide_stuff():
-	for i in get_children():
-		i.hide()
+	$BackGroundColorRect.hide()
+	$StartButton.hide()
+	$GameNameLabel.hide()
+	
+func show_stuff():
+	$HealthBar.show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+	
+func _physics_process(delta):
 	pass
 
 
 func _on_start_button_button_up():
 	hide_stuff()
+	show_stuff()
 	emit_signal("game_started")
 	
